ae750504635b19a9ac90fb7e136dd8aae87b1389
Larisa <lyeremyan@wovenmedia.com>
2025-10-22 14:06:54 +0400

    M5

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,27 +24,27 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'allure-playwright',
-  reporter: [['allure-playwright', {outputFolder: "GoogleAllureResults4"}]],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: "https://restful-booker.herokuapp.com",
-    extraHTTPHeaders: {
-      Accept: "application/json"
-    },
+    // baseURL: "https://restful-booker.herokuapp.com",
+    // extraHTTPHeaders: {
+    //   Accept: "application/json"
+    // },
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    screenshot: 'on',
+    video: 'on',
     trace: 'retain-on-failure',
-    headless: true,
-    testIdAttribute: 'data-test',
+    // headless: true,
+    // testIdAttribute: 'data-test',
     //storageState: "./playwright/.auth/auth.json",
   },
   timeout: 10000,
@@ -71,15 +71,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
